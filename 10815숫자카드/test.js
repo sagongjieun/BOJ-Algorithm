0f9d9e32e377d2c0264f61c139690617fb81e02c
sagongjieun <gonii0821@naver.com>
2022-02-04 00:15:35 +0900

    220204

--- 10815숫자카드/test.js
+++ 10815숫자카드/test.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\r\n");
 
+//이분탐색 함수
 function binarySearch(arr, num) {
   let start = 0;
   let middle = 0;
@@ -21,7 +22,7 @@ function binarySearch(arr, num) {
   return -1;
 }
 
-//상근이가 가지고 있는 카드배열
+//상근이가 가지고 있는 카드배열 오름차순 정렬
 let sangkeun = input[1]
   .split(" ")
   .map((i) => parseInt(i))
@@ -29,9 +30,11 @@ let sangkeun = input[1]
 //비교할 카드배열
 let compare = input[3].split(" ").map((i) => parseInt(i));
 
+//비교할 카드배열의 각 요소에 대해
 for (let i = 0; i < compare.length; i++) {
   if (binarySearch(sangkeun, compare[i]) === 1) compare[i] = 1;
-  else compare[i] = 0;
+  //상근이가 가지고 있는 카드에 있다면 비교할 카드배열의 해당 수는 1로 바꾸기
+  else compare[i] = 0; //없다면 0으로 바꾸기
 }
 
 console.log(compare.join(" "));
