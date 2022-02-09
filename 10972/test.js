d935f935bf85fde3d10934b35be3163e70a48b62
sagongjieun <gonii0821@naver.com>
2022-02-09 23:41:38 +0900

    20220209

--- 10972/test.js
+++ 10972/test.js
@@ -20,9 +20,15 @@ function getPermutation(arr, selectNumber) {
   return result;
 }
 
-let numbers = [];
-let prev = input[1].split(" ").map((i) => parseInt(i));
-console.log(prev);
+let prev = input[1]; //찾아야 하는 다음 순열의 이전 순열 (문제에서 주어지는 입력값 순열)
+let numbers = []; //1-N까지의 수를 담을 배열
 for (let i = 1; i <= parseInt(input[0]); i++) numbers.push(i);
-let answer = getPermutation(numbers, numbers.length);
-console.log(answer[-1]);
+let permutation = getPermutation(numbers, numbers.length); //모든 순열 구하기
+for (let i = 0; i < permutation.length; i++)
+  permutation[i] = permutation[i].join(" ");
+
+const prevIndex = permutation.indexOf(prev); //모든 순열에서 prev의 index
+if (prevIndex === permutation.length - 1) console.log(-1);
+else console.log(permutation[prevIndex + 1]);
+
+// 후기 : N의 최대값이 10000이기 때문에 완전탐색은 불가능
