e900ba5a19526c64b971e994369080edd53d3bf0
sagongjieun <gonii0821@naver.com>
2022-02-22 01:00:39 +0900

    220222

--- 4673셀프넘버/test.js
+++ 4673셀프넘버/test.js
@@ -2,7 +2,7 @@
 //https://www.acmicpc.net/problem/4673
 
 function selfNumber(n) {
-  let number = n;
+  let number = n; //생성자인 n copy
   let result = 0;
 
   //숫자를 10으로 나누며 각 자릿수를 result에 더함
@@ -17,7 +17,7 @@ function selfNumber(n) {
 const range = 10000;
 let selfNumbers = new Array(range + 1).fill(true); //0~10000까지의 index를 가지는 배열 생성하고 true로 초기화
 
-//i가 셀프넘버가 아니라면 false
+//selfNumber함수에 생성자 i를 인자로 줬을 때, return되는 수는 셀프넘버가 아니므로 false로 만들기
 for (let i = 0; i <= range; i++) {
   selfNumbers[selfNumber(i)] = false;
 }
